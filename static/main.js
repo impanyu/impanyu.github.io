a941685a0051415220cc901a8add12083dde893e
yupan <impanyu@gmail.com>
2015-03-09 03:13:19 -0500

    8

--- static/main.js
+++ static/main.js
@@ -16,9 +16,11 @@ if (canvas.getContext) {
     //var enemyPosY=r;
     // var vX=0,vY=0,enemyVX=0,enemyVY=0;
      var getPos=false;
+     var playonce=0;
      //var arrow={u:0,r:0,d:0,l:0};
      var mine=1;
      var enemy=0;
+     var t=0;
     
     var tanks=[];
       tanks[0]=new myObjects.tank(50,canvas.height/2,"blue");
@@ -41,7 +43,11 @@ if (canvas.getContext) {
 
 
 //main game loop 
+
 setInterval(function() {
+
+    //if(t%2===0 && sendChannel && sendChannel.readyState==='open')
+       //sendChannel.send( JSON.stringify({'myscore':tanks[mine].score}));
     context.fillStyle = "#F0F0F0";
     context.fillRect(0,0,canvas.width, canvas.height);  
 
@@ -107,26 +113,50 @@ setInterval(function() {
       
     }
 
+   
    //judge if the game is over
     if(tanks[mine].score<=0){//you lose
       if(endMarks[1].timer<=150){
+        if(playonce===0)
         document.getElementById("lose").play();
+        playonce=1;
+        
+        deactivateKey();
        endMarks[1].render(context);
      }
       else{
+        t=0;
+        playonce=0;
+        activateKey();
         endMarks[1].timer=0;
+        //tanks[0].originX=50;
+        //tanks[0].originY=canvas.height/2;
+        //tanks[1].originX=canvas.width-50;
+        //tanks[1].originY=canvas.height/2;
         tanks[0]=new myObjects.tank(50,canvas.height/2,"blue");
         tanks[1]=new myObjects.tank(canvas.width-50,canvas.height/2,"red");
+        
       }
     }
        
     else if(tanks[enemy].score<=0){//you win
       if(endMarks[0].timer<=150){
+         if(playonce===0)
         document.getElementById("win").play();
+        playonce=1;
+
+         deactivateKey();
        endMarks[0].render(context);
      }
       else{
+        t=0;
+        playonce=0;
+        activateKey();
         endMarks[0].timer=0;
+       // tanks[0].originX=50;
+        //tanks[0].originY=canvas.height/2;
+        //tanks[1].originX=canvas.width-50;
+        //tanks[1].originY=canvas.height/2;
         tanks[0]=new myObjects.tank(50,canvas.height/2,"blue");
         tanks[1]=new myObjects.tank(canvas.width-50,canvas.height/2,"red");
       }
